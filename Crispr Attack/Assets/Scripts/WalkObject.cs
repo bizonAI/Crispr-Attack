a8c7e1bba7e9663fdb032f273f8f12273d1f2615
bizonAI <d.robra@online.de>
2019-02-24 15:37:34 +0100

    standard assets

---
+++ Crispr Attack/Assets/Scripts/WalkObject.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WalkObject : MonoBehaviour
+{
+    public GameObject camera;
+    public Vector3 moveToPos;
+    public float camMoveSpeed = 5;
+
+    bool wasHit;
+
+    private void Update()
+    {
+        if (wasHit)
+        {
+            camera.transform.position = Vector3.MoveTowards(camera.transform.position, moveToPos, camMoveSpeed * Time.deltaTime);
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        wasHit = true;
+    }
+}
